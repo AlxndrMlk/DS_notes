43082c707bf446f1b981bb8db4c1068b7369c7d6
Aleksander Molak <aleksander.molak@gmail.com>
2018-10-30 20:10:04 +0100

    Update keras_reg.py

--- keras_reg.py
+++ keras_reg.py
@@ -3,17 +3,18 @@ import keras
 from keras.layers import Dense
 from keras.models import Sequential
 
-# Save the number of columns in predictors: n_cols
+# Specify the model
 n_cols = predictors.shape[1]
-
-# Set up the model: model
 model = Sequential()
-
-# Add the first layer
-model.add(Dense(50, activation='relu', input_shape=(n_cols, )))
-
-# Add the second layer
-model.add(Dense(32, activation='relu', input_shape=(n_cols, )))
-
-# Add the output layer
+model.add(Dense(50, activation='relu', input_shape = (n_cols,)))
+model.add(Dense(32, activation='relu'))
 model.add(Dense(1))
+
+# Compile the model
+model.compile(loss='mean_squared_error', optimizer='adam')
+
+# Fit the model
+model.fit(predictors, target)
+
+# Predict 
+model.predict(test_data)
