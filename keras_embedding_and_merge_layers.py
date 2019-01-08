3bebf995ab47de58c30c556e8b540b1898e54eb8
Aleksander Molak <aleksander.molak@gmail.com>
2019-01-08 19:48:15 +0100

    Update keras_embedding_and_merge_layers.py

--- keras_embedding_and_merge_layers.py
+++ keras_embedding_and_merge_layers.py
@@ -48,3 +48,17 @@ model = Model([team_in_1, team_in_2], score_diff)
 
 # Compile the model
 model.compile('adam', 'mean_absolute_error')
+
+# Get the team_1 column from the regular season data
+input_1 = games_season['team_1']
+
+# Get the team_2 column from the regular season data
+input_2 = games_season['team_2']
+
+# Fit the model to input 1 and 2, using score diff as a target
+model.fit([input_1, input_2],
+          games_season['score_diff'],
+          epochs=1,
+          batch_size=2048,
+          validation_split=.1,
+          verbose=True)
