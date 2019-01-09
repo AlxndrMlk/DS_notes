36c243459f0311c63e84d239617f3ddfdc915327
Aleksander Molak <aleksander.molak@gmail.com>
2019-01-09 19:03:10 +0100

    Update keras_3_input_model.py

--- keras_3_input_model.py
+++ keras_3_input_model.py
@@ -21,3 +21,28 @@ model = Model([team_in_1, team_in_2, home_in], out)
 
 # Compile the model
 model.compile(optimizer='adam', loss='mean_absolute_error')
+
+# Predict
+games_tourney['pred'] = model.predict([games_tourney['team_1'], games_tourney['team_2'], games_tourney['home']])
+
+# Create an input layer with 3 columns
+input_tensor = Input((3,))
+
+# Pass it to a Dense layer with 1 unit
+output_tensor = Dense(1)(input_tensor)
+
+# Create a model
+model = Model(input_tensor, output_tensor)
+
+# Compile the model
+model.compile(optimizer='adam', loss='mean_absolute_error')
+
+# Fit the model
+model.fit(games_tourney_train[['home', 'seed_diff', 'pred']],
+          games_tourney_train['score_diff'],
+          epochs=1,
+          verbose=True)
+
+# Evaluate the model on the games_tourney_test dataset
+model.evaluate(games_tourney_test[['home', 'seed_diff', 'prediction']], 
+               games_tourney_test['score_diff'])
